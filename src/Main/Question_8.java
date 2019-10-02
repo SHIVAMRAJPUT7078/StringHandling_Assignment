1c4090aabde8e049aec7fa0d60b6e737b31b3e46
shivam rajput <shivam.rajput_cs18@gmail.com>
2019-10-02 11:27:52 +0530

    Add statements for question_8

--- src/Main/Question_8.java
+++ src/Main/Question_8.java
@@ -1,4 +1,14 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Question_8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String Str = scanner.nextLine().trim();
+        String Str1 = Str.toUpperCase();
+        Question_7 question_7 = new Question_7(Str1);
+        System.out.println("Total weight in Upper cas is ; " + question_7.totalweight());
+    }
 }
